8060dfc8997505dbc196196c28f9ed10f8c056c7
skycloudstar-coding <122344783+skycloudstar-coding@users.noreply.github.com>
2025-01-12 14:20:14 +0900

    Update todos.py

--- todos.py
+++ todos.py
@@ -24,4 +24,9 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_name": item.name, "item_id": item_id
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id
