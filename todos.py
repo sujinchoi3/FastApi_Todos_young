ca11375dad39aa27963cf684ac0d1dea465f24c2
skycloudstar-coding <122344783+skycloudstar-coding@users.noreply.github.com>
2025-01-12 15:14:28 +0900

    Update todos.py

--- todos.py
+++ todos.py
@@ -26,7 +26,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id
 
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port = 8000)
